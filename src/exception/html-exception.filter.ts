64a1dacdcc1f5dc6b3703d8362ff4e94dc17da97
wlstmd <jinwoo8288@gmail.com>
2024-07-16 10:58:29 +0900

    feat: exception design

--- src/exception/html-exception.filter.ts
+++ src/exception/html-exception.filter.ts
@@ -11,42 +11,46 @@ export class HtmlExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus();
     const message = exception.message;
 
-    response.status(status).send(`
-        <html>
-          <head>
-            <title>Error</title>
-            <style>
-              body {
-                font-family: Arial, sans-serif;
-                background-color: #000000;
-                color: #ffffff;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-              }
-              .error-container {
-                text-align: center;
-                padding: 20px;
-                border-radius: 10px;
-                background-color: #333333;
-              }
-              h2 {
-                color: #ff4444;
-              }
-            </style>
-          </head>
-          <body>
-            <div class="error-container">
-              <h2>Error ${status}</h2>
-              <p>${message}</p>
-            </div>
-          </body>
-        </html>
-      `);
+    response.send(`
+      <html>
+        <head>
+          <title>Error</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #f0f0f0;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              text-align: center;
+              height: 100vh;
+              margin: 0;
+            }
+            .error-container {
+              text-align: center;
+              padding: 20px;
+            }
+            h2 {
+              color: #3080C6;
+            }
+            p {
+              color: #3080C6;
+            }
+            img {
+              width: 120px;
+              margin: 5px;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="error-container">
+            <img src="https://github.com/GBSWHS/CI-Signature/raw/main/symbol/symbol-solid.png" alt="경북소프트웨어고등학교 로고">
+            <p>${message}</p>
+          </div>
+        </body>
+      </html>
+    `);
   }
 }
